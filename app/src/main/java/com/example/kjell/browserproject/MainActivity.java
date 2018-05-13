f195347763619e207eaa2d3a62094042b53cd22c
kjelldg <Kjelldg@gmail.com>
2018-05-13 21:17:56 +0200

    Minor changes.

--- app/src/main/java/com/example/kjell/browserproject/MainActivity.java
+++ app/src/main/java/com/example/kjell/browserproject/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.kjell.browserproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button go;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        go = (Button) findViewById(R.id.btn_go);
+        text = (TextView) findViewById(R.id.tv_text);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+
 }
