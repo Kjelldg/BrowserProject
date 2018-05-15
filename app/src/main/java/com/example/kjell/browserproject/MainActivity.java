a3a7176de7ff8bd2426dcde8757e33f887947cd6
Kjell Denti <kjelldg@gmail.com>
2018-05-15 23:15:21 +0200

    Web browsing now possible.

--- app/src/main/java/com/example/kjell/browserproject/MainActivity.java
+++ app/src/main/java/com/example/kjell/browserproject/MainActivity.java
@@ -3,28 +3,53 @@ package com.example.kjell.browserproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button go;
-    TextView text;
+    WebView webView_Browser;
+    EditText editText_Url;
+    Button forward_Button, backward_Button, refresh_Button, clear_Button, go_Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        go = (Button) findViewById(R.id.btn_go);
-        text = (TextView) findViewById(R.id.tv_text);
 
-        go.setOnClickListener(new View.OnClickListener() {
+
+        webView_Browser = (WebView) findViewById(R.id.webView_Browser);
+
+        webView_Browser.setWebViewClient(new WebViewClient());
+
+        editText_Url = (EditText) findViewById(R.id.editText_Url);
+
+        go_Button = (Button) findViewById(R.id.go_Button);
+        forward_Button = (Button) findViewById(R.id.forward_Button);
+        backward_Button = (Button) findViewById(R.id.backward_Button);
+        refresh_Button = (Button) findViewById(R.id.refresh_Button);
+        clear_Button = (Button) findViewById(R.id.clear_Button);
+
+        go_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String editTextValue = editText_Url.getText().toString();
+                String url_String = editTextValue;
+                webView_Browser.loadUrl(url_String);
             }
         });
+
+
+        // webView_Browser.setWebViewClient(new WebViewClient());
+
+        //String url_String = "http://www.computersweden.idg.se";
+
+        //webView_Browser.loadUrl(url_String);
     }
 
 
